64d241d99e1d9f32b6606db888d57ca0066718e4
husnul <husnuljahneer19@gmail.com>
2022-10-25 17:47:16 +0530

    added scriptiing

--- script/script.js
+++ script/script.js
@@ -1,24 +1,25 @@
-function onload() {
-  const petList = [
-    {
-      id: 1,
-      name: "dog",
-      url: "./pets/bat.gif",
-      width: 100,
-      height: 100
-    },
-    {
-      id: 2,
-      name: "croc",
-      url: "./pets/crocs.gif",
-      width: 100,
-      height: 100
-    }
-  ];
+function ssh() {
+  function onload() {
+    const petList = [
+      {
+        id: 1,
+        name: "dog",
+        url: "./pets/bat.gif",
+        width: 100,
+        height: 100
+      },
+      {
+        id: 2,
+        name: "croc",
+        url: "./pets/crocs.gif",
+        width: 100,
+        height: 100
+      }
+    ];
 
-  document.getElementById("pet_row").innerHTML = petList
-    .map(
-      (pet) => `
+    document.getElementById("pet_row").innerHTML = petList
+      .map(
+        (pet) => `
         <center>
         <div style="margin-top:-50px;margin-left:30px">
         <table>
@@ -28,8 +29,8 @@ function onload() {
             </td>
             <td align="center">
               <img src="${pet.url}" width="${pet.width}" height="${
-        pet.height
-      }" class="img_style" />
+          pet.height
+        }" class="img_style" />
             </td>
             <td>
               <button onclick="chooseMe('${
@@ -40,18 +41,28 @@ function onload() {
         </table>
         </div>
         </center>`
-    )
-    .join("");
+      )
+      .join("");
+  }
+
+  function chooseMe(petUrl) {
+    let headerHide = document.getElementById("header");
+    let pet = document.getElementById("pet_row");
+    pet.style.display = "none";
+    headerHide.style.display = "none";
+    let petImage = document.createElement("img");
+    petImage.src = petUrl;
+    petImage.width = "150";
+    petImage.height = "150";
+    document.body.appendChild(petImage);
+  }
+  window.addEventListener("load", onload);
 }
 
-function chooseMe(petUrl) {
-  let headerHide = document.getElementById("header");
-  let pet = document.getElementById("pet_row");
-  pet.style.display = "none";
-  headerHide.style.display = "none";
-  let petImage = document.createElement("img");
-  petImage.src = petUrl;
-  petImage.width = "150";
-  petImage.height = "150";
-  document.body.appendChild(petImage);
-}
+let script = document.createElement("script");
+script.type = "text/javascript";
+script.text = `(${ssh.toString()})()`;
+script.onload = function () {
+  this.parentNode.removeChild(this);
+};
+(document.head || document.documentElement).appendChild(script);
