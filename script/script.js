87bb7b044681e56de409117df79670b6ccc63c7f
husnul <husnuljahneer19@gmail.com>
2022-10-25 17:27:06 +0530

    Added Script

--- script/script.js
+++ script/script.js
@@ -1,81 +1,57 @@
-function onLoadRun() {
-  let petList = [
+function onload() {
+  const petList = [
     {
+      id: 1,
       name: "dog",
-      url: "./pets/animation_500_l9ny6hk3.gif",
+      url: "./pets/bat.gif",
       width: 100,
-      height: 100,
-      top: 0,
-      left: 0,
-      speed: 1,
-      direction: "right"
+      height: 100
     },
     {
-      name: "cat",
-      url: "./pets/animation_500_l9ny6hk3.gif",
+      id: 2,
+      name: "croc",
+      url: "./pets/crocs.gif",
       width: 100,
-      height: 100,
-      top: 0,
-      left: 0,
-      speed: 1,
-      direction: "right"
+      height: 100
     }
   ];
-
-  function chooseMe() {
-    let pet = document.getElementById("pet_row");
-    pet.style.display = "none";
-    let petImage = document.createElement("img");
-    petImage.src = "./pets/animation_500_l9ny6hk3.gif";
-    petImage.width = "100";
-    petImage.height = "100";
-    document.body.appendChild(petImage);
-    updatePosition(100, 100);
-    // test();
-  }
 
   document.getElementById("pet_row").innerHTML = petList
     .map(
       (pet) => `
-            <center>
-            <table>
-              <tr>
-                <td>
-                      <p>${pet.name}</p>
-                </td>
-                <td>
-                      <img src="${pet.url}" width="${pet.width}" height="${pet.height}" style="margin-bottom:55px" />
-                </td>
-                <td>
-                      <button onclick="chooseMe('${pet.name}')" style="background-color: #4CAF50; /* Green */ border: none; color: white; padding: 5px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px;">Choose Me</button>
-                </td>
-              </tr>
-            </table>
-            </center>
-                    `
+        <center>
+        <div style="margin-top:-50px;margin-left:30px">
+        <table>
+          <tr>
+            <td>
+              <p style="font-weight:bold;">${pet.name.toUpperCase()}</p>
+            </td>
+            <td align="center">
+              <img src="${pet.url}" width="${pet.width}" height="${
+        pet.height
+      }" class="img_style" />
+            </td>
+            <td>
+              <button onclick="chooseMe('${
+                pet.url
+              }')" class="button_primary">Choose Me</button>
+            </td>
+          </tr>
+        </table>
+        </div>
+        </center>`
     )
     .join("");
+}
 
-  var petImage = document.createElement("img");
-
-  function test() {
-    console.log("test");
-    let petImage = document.createElement("img");
-    let petLeft = 0;
-    let petTop = 0;
-    let petSpeed = 10;
-    let petDirection = "right";
-    let petWidth = 1;
-    petLeft += petSpeed;
-    petImage.style.marginTop = petLeft + "px";
-    // setInterval(test, 100);
-  }
-  const position = {
-    x: 0,
-    y: 0
-  };
-  function updatePosition(x, y) {
-    petImage.style.left = (position.x = x) + "px";
-    petImage.style.top = (position.y = y) + "px";
-  }
+function chooseMe(petUrl) {
+  let headerHide = document.getElementById("header");
+  let pet = document.getElementById("pet_row");
+  pet.style.display = "none";
+  headerHide.style.display = "none";
+  let petImage = document.createElement("img");
+  petImage.src = petUrl;
+  petImage.width = "150";
+  petImage.height = "150";
+  document.body.appendChild(petImage);
 }
